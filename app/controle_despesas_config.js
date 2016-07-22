d56c49e7e2592e89044d09348c752bc3029d1516
Ivan Alves <x.surian.x@gmail.com>
2016-07-22 15:56:36 -0300

    configurado rotas para carregarem os controllers usando o ocLazyLoad

--- app/controle_despesas_config.js
+++ app/controle_despesas_config.js
@@ -5,19 +5,18 @@
         .module('controleDespesa').config(ConfigurarProjeto);
 
     function ConfigurarProjeto($stateProvider, $urlRouterProvider){
+
+        var login = {
+            url: '/login',
+            templateUrl: 'app/views/login/login.html',
+            resolve: {
+                deps: function($ocLazyLoad){
+                    return $ocLazyLoad.load('app/views/login/login-controller.js');
+                }
+            }
+        };
+
+        $stateProvider.state('login', login);
         $urlRouterProvider.otherwise('/login');
-
-        // criar rotas das paginas
-        $stateProvider
-            .state('login', {
-                url: "/login",
-                templateUrl: "app/views/login/login.html"
-            });
-
-        $stateProvider
-            .state('lembrarSenha', {
-                url: "/lembrarSenha",
-                templateUrl: "app/views/login/login.html"
-            });
     }
 })();
